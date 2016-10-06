bc6e2c70bf09a7f29076daabbbe378997d79739a
Andrei Visan <andrei.m.visan@gmail.com>
2016-10-06 16:38:39 +0200

    Added Chrome Custom Tabs

--- app/src/main/java/com/adyen/dl/PaymentRedirectActivity.java
+++ app/src/main/java/com/adyen/dl/PaymentRedirectActivity.java
@@ -1,57 +1,97 @@
 package com.adyen.dl;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.support.customtabs.CustomTabsCallback;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+import android.text.TextUtils;
+
+import com.adyen.dl.shared.CustomTabsHelper;
+import com.adyen.dl.shared.ServiceConnectionCallback;
 
 /**
  * Created by andrei on 9/15/16.
  */
-public class PaymentRedirectActivity extends Activity {
+public class PaymentRedirectActivity extends Activity implements ServiceConnectionCallback {
 
-    private WebView mPaymentWebView;
+    private CustomTabsClient mClient;
+    private CustomTabsServiceConnection mConnection;
 
-    private Context context;
+    private String mPackageNameToBind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_redirect);
 
-        context = this;
-
         Intent resultIntent = getIntent();
         String redirectUrl = resultIntent.getStringExtra("redirectUrl");
 
-        mPaymentWebView = (WebView)findViewById(R.id.paymentWebView);
-        mPaymentWebView.setWebViewClient(new CustomWebBrowser());
-
-        mPaymentWebView.getSettings().setLoadsImagesAutomatically(true);
-        mPaymentWebView.getSettings().setJavaScriptEnabled(true);
-        mPaymentWebView.getSettings().setSupportMultipleWindows(true);
-        mPaymentWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        mPaymentWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-
-        mPaymentWebView.loadUrl(redirectUrl);
+        bindCustomTabsService(redirectUrl);
     }
 
-    private class CustomWebBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if(url.startsWith("adyendl")) {
-                Intent intent = new Intent(context, PaymentResultActivity.class);
-                intent.putExtra("callbackUrl", url);
-                startActivity(intent);
-                finish();
-            } else {
-                view.loadUrl(url);
+    private void bindCustomTabsService(final String url) {
+        if (mClient != null) return;
+        if (TextUtils.isEmpty(mPackageNameToBind)) {
+            mPackageNameToBind = CustomTabsHelper.getPackageNameToUse(this);
+            if (mPackageNameToBind == null) {
+                return;
+            }
+        }
+
+        mConnection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
+                mClient = customTabsClient;
+                prefetchContent(url);
+                loadCustomTabs(url);
             }
 
-            return true;
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                mClient = null;
+            }
+        };
+
+        CustomTabsClient.bindCustomTabsService(this, mPackageNameToBind, mConnection);
+    }
+
+    public void prefetchContent(String url) {
+        if (mClient != null) {
+            mClient.warmup(0);
+            CustomTabsSession customTabsSession = getSession();
+            customTabsSession.mayLaunchUrl(Uri.parse(url), null, null);
         }
+    }
+
+    public void loadCustomTabs(String url) {
+        CustomTabsIntent.Builder mBuilder = new CustomTabsIntent.Builder(getSession());
+        CustomTabsIntent mIntent = mBuilder.build();
+        mIntent.launchUrl(this, Uri.parse(url));
+    }
+
+    private CustomTabsSession getSession() {
+        return mClient.newSession(new CustomTabsCallback() {
+            @Override
+            public void onNavigationEvent(int navigationEvent, Bundle extras) {
+                super.onNavigationEvent(navigationEvent, extras);
+            }
+        });
+    }
+
+    @Override
+    public void onServiceConnected(CustomTabsClient client) {
+        mClient = client;
+    }
+
+    @Override
+    public void onServiceDisconnected() {
+        mClient = null;
     }
 }
--- app/src/main/java/com/adyen/dl/PaymentResultActivity.java
+++ app/src/main/java/com/adyen/dl/PaymentResultActivity.java
@@ -2,6 +2,7 @@ package com.adyen.dl;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -20,11 +21,11 @@ public class PaymentResultActivity extends Activity {
         setContentView(R.layout.activity_payment_result);
 
         Intent intent = getIntent();
+        Uri callbackUri = intent.getData();
 
         mResultTextView = (TextView) findViewById(R.id.result);
 
-        String resultUrl = intent.getStringExtra("callbackUrl");
-        String response = PaymentsProcessor.getInstance().verifyResultUrl(resultUrl);
+        String response = PaymentsProcessor.getInstance().verifyResultUrl(callbackUri.toString());
 
         mResultTextView.setText(response);
 
